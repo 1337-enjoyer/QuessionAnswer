df62fa18a00524a10ec56778b15b270ca374cb00
Anton Silaev <s1laev-aa@yandex.ru>
2025-11-22 22:34:47 +0300

    First commit

---
+++ ques_answ/main_app/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):  # HttpRequest
+    return HttpResponse('Ответы на вопросы')

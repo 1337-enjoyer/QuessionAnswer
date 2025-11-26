4fd6d014fbf8191440afeb9962e475d4366752e6
Anton Silaev <s1laev-aa@yandex.ru>
2025-11-26 19:23:32 +0300

    Migrations, ORM work with shell_plus

--- ques_answ/main_app/models.py
+++ ques_answ/main_app/models.py
@@ -5,4 +5,8 @@ class Question(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
     time_create = models.DateTimeField(auto_now_add=True)
-    rating = models.IntegerField(default=0)
+    count_answers = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+    

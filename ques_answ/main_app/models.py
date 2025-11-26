4d85e1cd3ede6ff079e84c1c077c4f391f0ebb87
Anton Silaev <s1laev-aa@yandex.ru>
2025-11-26 15:50:13 +0300

    Migrations. Start CRUD learning

--- ques_answ/main_app/models.py
+++ ques_answ/main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    time_create = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)

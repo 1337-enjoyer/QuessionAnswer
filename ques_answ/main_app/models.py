5f4386865928f3410aed0eacfc046a0de87268d0
Anton Silaev <s1laev-aa@yandex.ru>
2025-11-26 20:06:24 +0300

    Migration and Meta class for ORM

--- ques_answ/main_app/models.py
+++ ques_answ/main_app/models.py
@@ -6,7 +6,13 @@ class Question(models.Model):
     content = models.TextField(blank=True)
     time_create = models.DateTimeField(auto_now_add=True)
     count_answers = models.IntegerField(default=0)
+    is_published: models.BooleanField = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
-    
+
+    class Meta:
+        ordering = ['-time_create']
+        indexes = [
+            models.Index(fields=['-time_create'])
+        ]
